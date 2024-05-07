e78b97609fa6d92f8198a33901cbd5be92041906
末次 <syunjulee@gmail.com>
2024-05-07 15:48:25 +0800

    Issue #4: schedules, core/urls, templates

--- schedules/urls.py
+++ schedules/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
+from . import views
 
+app_name = "schedules"
+
+urlpatterns = [
+    path("", views.home, name="index"), # 行程首頁
+    path("build", views.new, name="new"), # 輸入行程資料的頁面
+    path("add", views.create, name="add"), # 寫入資料庫的觸發按鈕
+    # path("<id>/edit", views.edit, name="edit"), # 未做
+    # path("<id>/delete", views.delete, name="delete"), # 未做
+    # path("<id>", views.show, name="show") # 連到 spots
+]
--- schedules/views.py
+++ schedules/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # 渲染和轉址
+from django.views.decorators.http import require_POST # 判斷是不是 POST
+from .models import Schedules # 從同家的 model 取 class
 
-# Create your views here.
+
+# 列出行程
+def home(req):
+    schedules = Schedules.objects.all()
+    return render(req, "schedules/index.html", {"schedules":schedules})
+
+# 新增行程
+def new(req):
+    return render(req, "schedules/build.html")
+
+# 寫入資料庫
+@require_POST
+def create(req):
+    schedule = Schedules(
+        name=req.POST["name"],
+        start_date=req.POST["start_date"],
+        end_date=req.POST["end_date"],
+        transportation=req.POST["transportation"] 
+    )
+    schedule.save()
+        
+    return redirect("root")
