845d34586748877675b2ada125a55ab4141b7109
末次 <syunjulee@gmail.com>
2024-05-07 12:58:32 +0800

    delete: old branch files & update: models.py

--- schedules/models.py
+++ schedules/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
-def Schedules():
-  pass
+class Schedules(models.Model):
+    name = models.CharField(max_langth=100)
+    start_date = models.DateTimeField(auto_now=True)
+    ended_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(default="")
