3ac9ba84c32336fb9094cfa606654f0073ad3e1d
Josh Lin <mingyi1223@gmail.com>
2024-05-07 12:01:06 +0800

    issue #1 update members

--- members/views.py
+++ members/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUp
 
 # Create your views here.
+
+
+# Login
+def login_user(req):
+    if req.method == "POST":
+        username = req.POST["username"]
+        password = req.POST["password"]
+
+        user = authenticate(req, username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(req, user)
+            return redirect("home")
+        else:
+            return redirect("login")
+    else:
+        return render(req, "registration/login.html")
+
+
+# Logout
+def logout_user(req):
+    logout(req)
+    return redirect("home")
+
+
+# Register
+def register_user(req):
+    if req.method == "POST":
+        form = SignUp(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            print(form.errors)
+    else:
+        form = SignUp()
+
+    return render(req, "registration/register.html", {"form": form})
