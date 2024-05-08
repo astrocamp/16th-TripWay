d94e9940f7ae8344f4a454977da12ff5f29aa50d
末次 <syunjulee@gmail.com>
2024-05-09 00:02:57 +0800

    Issue #4: tailwindcss apply

--- schedules/urls.py
+++ schedules/urls.py
@@ -4,12 +4,10 @@ from . import views
 app_name = "schedules"
 
 urlpatterns = [
-    path("", views.home, name="index"), # 行程首頁
+    path("", views.home, name="index"), # 行程列表
     path("new", views.new, name="new"), # 輸入行程資料的頁面
     path("add", views.create, name="add"), # 寫入資料庫的觸發按鈕
-    path('delete/<int:id>/', views.delete, name='delete'),
-    # path("<id>", views.show, name="show")
+    path('delete/<int:id>/', views.delete, name='delete'), # 刪除行程
     # path("<id>/edit", views.edit, name="edit"), # 未做
-    # path("<id>/delete", views.delete, name="delete"), # 未做
-    # path("<id>", views.show, name="show") # 連到 spots
+    # path("<id>", views.show, name="show") # 連到 spots:index
 ]
