2b5be1a896f2ee373bd1bb350981d75da2638eea
末次 <syunjulee@gmail.com>
2024-05-09 00:02:57 +0800

    Issue #4: tailwindcss apply

--- schedules/views.py
+++ schedules/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render, redirect, get_object_or_404 # 渲染轉址和判斷有沒有資料
-from django.views.decorators.http import require_POST # 判斷是不是 POST
-from .models import Schedules # 從同家的 model 取 class
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from .models import Schedules
 
 
-# 列出行程
+# 列出目前行程
 def home(req):
     schedules = Schedules.objects.all()
     return render(req, "schedules/index.html", {"schedules":schedules})
 
-# 新增行程
+# 輸入行程資訊
 def new(req):
     return render(req, "schedules/new.html")
 
-# 寫入資料庫
+# 新增行程(寫入資料庫)
 @require_POST
 def create(req):
     schedule = Schedules(
@@ -25,7 +25,7 @@ def create(req):
         
     return redirect("schedules:index")
 
-# 刪除資料
+# 刪除行程(硬刪)
 @require_POST    
 def delete(req, id):
     schedule = get_object_or_404(Schedules, pk=id)
