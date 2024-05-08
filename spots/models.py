58149632b966d0dfe06796aa5e0e49b17b53f54d
FangFangss <cjiyvonne@gmail.com>
2024-05-08 20:38:48 +0800

    issue #5 update spots/models.py and views.py

--- spots/models.py
+++ spots/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class spots_list(models.Model):
     spot_name = models.CharField(max_length=100)
@@ -8,3 +9,10 @@ class spots_list(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True)
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.spot_name
