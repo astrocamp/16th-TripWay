570c386eb0740be3fe217f1c82ffbfa6e5d8529f
末次 <syunjulee@gmail.com>
2024-05-08 14:27:56 +0800

    Issue #4: fixed model.py

--- schedules/models.py
+++ schedules/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Schedules(models.Model):
     name = models.CharField(max_length=100)
-    start_date = models.DateTimeField(default="")
-    end_date = models.DateTimeField(default="")
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     # TRANSPORT_CHOICES = [
     #     ('walk', '走路'),
     #     ('public', '大眾運輸'),
@@ -11,3 +11,6 @@ class Schedules(models.Model):
     #     ('motorcycle', '機車'),
     # ]
     transportation = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
