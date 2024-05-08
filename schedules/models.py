9a11473cbcab43ee6256c19f3e5d396c7c0930cf
末次 <syunjulee@gmail.com>
2024-05-09 00:02:57 +0800

    Issue #4: tailwindcss apply

--- schedules/models.py
+++ schedules/models.py
@@ -4,12 +4,6 @@ class Schedules(models.Model):
     name = models.CharField(max_length=100)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    # TRANSPORT_CHOICES = [
-    #     ('walk', '走路'),
-    #     ('public', '大眾運輸'),
-    #     ('car', '汽車'),
-    #     ('motorcycle', '機車'),
-    # ]
     transportation = models.CharField(max_length=20)
     # created_at = models.DateTimeField(auto_now_add=True)
     # updated_at = models.DateTimeField(auto_now=True)
