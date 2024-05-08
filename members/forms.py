7a82e410712e1d52460380e522f2800a025f0a3d
Josh Lin <mingyi1223@gmail.com>
2024-05-08 14:01:57 +0800

    issue#14 Upadate Members: Google Login

--- members/forms.py
+++ members/forms.py
@@ -4,39 +4,37 @@ from django.contrib.auth.models import User
 
 
 class SignUp(UserCreationForm):
-    username = forms.CharField(
-        label="帳號",
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none"
-            }
-        ),
-    )
-    # email = forms.EmailField(
-    #     label="電子郵件",
-    #     widget=forms.EmailInput(
-    #         attrs={
-    #             "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none"
-    #         }
-    #     ),
-    # )
-    password1 = forms.CharField(
-        label="密碼",
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none"
-            }
-        ),
-    )
-    password2 = forms.CharField(
-        label="密碼確認",
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none"
-            }
-        ),
-    )
+    # username = forms.CharField(required=False)
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        labels = {
+            "username": "帳號",
+            "email": "信箱",
+            "password1": "密碼",
+            "password2": "密碼確認",
+        }
+        widgets = {
+            "username": forms.TextInput(
+                attrs={
+                    "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none"
+                }
+            ),
+            "email": forms.EmailInput(
+                attrs={
+                    "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none"
+                }
+            ),
+            "password1": forms.PasswordInput(
+                attrs={
+                    "autocomplete": "new-password",
+                    "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none",
+                }
+            ),
+            "password2": forms.PasswordInput(
+                attrs={
+                    "class": "form-control w-72 px-2 py-2 border-2 rounded-lg focus:outline-none"
+                }
+            ),
+        }
