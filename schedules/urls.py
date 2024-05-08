b3310244d8851b0dbe1f3588c7418b6a54820443
末次 <syunjulee@gmail.com>
2024-05-08 17:05:19 +0800

    Issue #4: index,new,add

--- schedules/urls.py
+++ schedules/urls.py
@@ -5,7 +5,7 @@ app_name = "schedules"
 
 urlpatterns = [
     path("", views.home, name="index"), # 行程首頁
-    path("build", views.new, name="new"), # 輸入行程資料的頁面
+    path("new", views.new, name="new"), # 輸入行程資料的頁面
     path("add", views.create, name="add"), # 寫入資料庫的觸發按鈕
     # path("<id>/edit", views.edit, name="edit"), # 未做
     # path("<id>/delete", views.delete, name="delete"), # 未做
