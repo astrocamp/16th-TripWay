e03193ccdebb9bac6ade851ddd53fc5c4b97a2a4
FangFangss <cjiyvonne@gmail.com>
2024-05-08 11:45:33 +0800

    add spots app

--- spots/models.py
+++ spots/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class spots_list(models.Model):
+    spot_name = models.CharField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField() 
+    note = models.TextField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
--- spots/urls.py
+++ spots/urls.py
@@ -1,1 +1,13 @@
 from django.urls import path
+from . import views
+
+app_name="Spot"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("new", views.new, name="new"),
+    path("add", views.create, name="add"),
+    path("<id>/update", views.update, name="update"),
+    path("<id>delete", views.delete, name="delete"),
+    path("<id>", views.show, name="show")
+]
