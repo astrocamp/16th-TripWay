05bca166218e22d2d7f2970309518a19bc153be0
Josh Lin <mingyi1223@gmail.com>
2024-05-09 19:05:05 +0800

    Issue#18 Update json

--- schedules/views.py
+++ schedules/views.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from .models import Schedules
-=======
-from django.shortcuts import render, redirect  # 渲染和轉址
-from django.views.decorators.http import require_POST  # 判斷是不是 POST
-from .models import Schedules  # 從同家的 model 取 class
->>>>>>> 87549ca (Issue#18 update)
 
 
 # 列出目前行程
@@ -18,12 +12,8 @@ def home(req):
 def new(req):
     return render(req, "schedules/new.html")
 
-<<<<<<< HEAD
-# 新增行程(寫入資料庫)
-=======
 
 # 寫入資料庫
->>>>>>> 87549ca (Issue#18 update)
 @require_POST
 def create(req):
     schedule = Schedules(
@@ -33,17 +23,13 @@ def create(req):
         transportation=req.POST["transportation"],
     )
     schedule.save()
-<<<<<<< HEAD
-        
+
     return redirect("schedules:index")
 
+
 # 刪除行程(硬刪)
-@require_POST    
+@require_POST
 def delete(req, id):
     schedule = get_object_or_404(Schedules, pk=id)
     schedule.delete()
-    return redirect('schedules:index')
-=======
-
-    return redirect("root")
->>>>>>> 87549ca (Issue#18 update)
+    return redirect("schedules:index")
