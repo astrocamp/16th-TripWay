1ff7cb276f96076ff20fb8af803ea895a5977a0d
FangFangss <cjiyvonne@gmail.com>
2024-05-09 17:08:27 +0800

    issue #5 Modify models.py class name and urls.py app_name

--- spots/models.py
+++ spots/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-class spots_list(models.Model):
+class SpotsList(models.Model):
     spot_name = models.CharField(max_length=100)
     start_time = models.TimeField()
     end_time = models.TimeField() 
