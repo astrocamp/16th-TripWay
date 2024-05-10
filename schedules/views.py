c3095d751673128884e5340ceef410d0fb33c42c
Josh Lin <mingyi1223@gmail.com>
2024-05-11 03:17:04 +0800

    fix bug

--- schedules/views.py
+++ schedules/views.py
@@ -3,14 +3,15 @@ from django.views.decorators.http import require_POST
 from .models import Schedule
 from trips.models import Trip
 from django.utils import timezone
+from django.urls import reverse
 
 
 def index(request, id):
     schedules = Schedule.objects.all().filter()
     # schedules = spots_list.objects.all().order_by("start_time")
-    trips = get_object_or_404(Trip, pk=id)
+    trip = get_object_or_404(Trip, pk=id)
     return render(
-        request, "schedules/index.html", {"schedules": schedules, "trips": trips}
+        request, "schedules/index.html", {"schedules": schedules, "trip": trip}
     )
 
 
@@ -20,7 +21,7 @@ def new(request, id):
 
 @require_POST
 def create(request, id):
-    trips = get_object_or_404(Trip, id=id)
+    trip = get_object_or_404(Trip, id=id)
 
     schedules = Schedule(
         # date=request.POST["date"],
@@ -28,15 +29,17 @@ def create(request, id):
         start_time=request.POST["start_time"],
         end_time=request.POST["end_time"],
         note=request.POST["note"],
+        trip=trip,
     )
 
     schedules.save()
 
-    return redirect(f"/trips/{trips.id}/schedules")
+    return redirect(reverse("trips:schedules:index", kwargs={"id": trip.id}))
 
 
 def show(request, id):
     schedules = get_object_or_404(Schedule, pk=id)
+    trip = schedules.trip
     if request.method == "POST":
         schedules.spot_name = request.POST["spot_name"]
         schedules.start_time = request.POST["start_time"]
@@ -45,7 +48,9 @@ def show(request, id):
         schedules.save()
         return redirect("schedules:show", id=id)
     else:
-        return render(request, "schedules/show.html", {"schedules": schedules})
+        return render(
+            request, "schedules/show.html", {"schedules": schedules, "trip": trip}
+        )
 
 
 def update(request, id):
