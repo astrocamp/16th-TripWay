fa64433f81f45892e0821a77ac9c1ccc7ee18c23
末次 <syunjulee@gmail.com>
2024-05-10 10:58:15 +0800

    修復關聯

--- spots/urls.py
+++ spots/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("new", views.new, name="new"),
     path("add", views.create, name="add"),
     path("<id>/update", views.update, name="update"),
-    path("<id>delete", views.delete, name="delete"),
+    path("<id>/delete", views.delete, name="delete"),
 ]
