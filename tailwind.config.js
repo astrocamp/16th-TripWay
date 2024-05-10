7f3fc8d6d812ede3d56e7b4a3f8ed6f821f35f53
Sean Chen <seanchen1073@gmail.com>
2024-05-10 17:04:55 +0800

    Issue #6 modified home,navbar,footer,base,tailwind.config.js,package-lock.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': '#85B6FF',
+        'blue': '#EBF0F6',
+        'btn-blue': '#649DFF',
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
