00d66ad6d4a407986313c61a06851c159e7f1d4a
末次 <syunjulee@gmail.com>
2024-05-10 11:31:58 +0800

    Issue #4 回歸settings和schedules/models

--- core/settings.py
+++ core/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "TripWay",
-        "USER": "yunju",
-        "PASSWORD": "password",
+        "USER": "os.getenv(YOUR_USER_NAME)",
+        "PASSWORD": "os.getenv(YOUR_PASSWORD)",
         "HOST": "localhost",
         "PORT": "5432",
     }
--- schedules/models.py
+++ schedules/models.py
@@ -3,12 +3,13 @@ from django.utils import timezone
 from members.models import Members
 
 class Schedules(models.Model):
-    members = models.ForeignKey(Members, on_delete=models.CASCADE, null=True)
+    members = models.ForeignKey(Members, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(default=timezone.now)
     transportation = models.CharField(max_length=20)
 
-
+    def __str__(self):
+        return self.spot_name
 
 # members/1/schedule/1/spots/index.html
