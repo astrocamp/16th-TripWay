9d0bf3c1675f62cbefc18e2f2f2236a05c146e32
chenfarng <chenfarng@gmail.com>
2024-05-13 16:43:43 +0800

    build member-trip relation

--- trips/models.py
+++ trips/models.py
@@ -4,7 +4,7 @@ from members.models import Members
 
 
 class Trip(models.Model):
-    # members = models.ForeignKey(Members, on_delete=models.CASCADE)
+    member = models.ManyToManyField(Members)
     name = models.CharField(max_length=100)
     start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(default=timezone.now)
