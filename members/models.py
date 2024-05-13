320d5f503466c49276b98131538673f7f4294546
末次 <syunjulee@gmail.com>
2024-05-13 16:55:45 +0800

    建立MemberSpot資料表

--- members/models.py
+++ members/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
+from django.utils import timezone
 
 class Members(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
+
+class MemberSpot(models.Model):
+    """
+    member = models.ForeignKey(Members, on_delete=models.CASCADE, related_name='members')
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE, related_name='spots')
+    """
+    pass
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
