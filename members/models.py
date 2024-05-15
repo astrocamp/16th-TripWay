b3f70dd5e96146ff21cb1ef869c5cd7e7b0f9124
末次 <syunjulee@gmail.com>
2024-05-15 15:57:31 +0800

    Issue #29 update: member model, views, urls

--- members/models.py
+++ members/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
+from django.utils import timezone
 
 
 class Member(AbstractUser):
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, 
+    unique=True)
+
+
+class MemberSpot(models.Model):
+    member = models.ForeignKey(
+        Member, on_delete=models.CASCADE, related_name="members"
+    )
+    spot = models.ForeignKey("Spot", on_delete=models.CASCADE, related_name='favorite_spots')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    class Meta:
+        unique_together = ('member', 'spot')
