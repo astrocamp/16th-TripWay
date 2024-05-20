c0bb1c4804c0c48dd268e2b8cf3fa58b8d7a3d06
Yuting Lin <ylin8266@gmail.com>
2024-05-20 12:17:32 +0800

    issue #3 update spots table

--- spots/form.py
+++ spots/form.py
@@ -5,9 +5,13 @@ from .models import Spot
 class SpotForm(forms.ModelForm):
     class Meta:
         model = Spot
-        fields = ["name", "city", "latitude", "longitude", "description"]
+        fields = ["name", "address", "latitude", "longitude", "phone", "url", "rating"]
         labels = {
             "name": "名稱",
-            "city": "城市",
-            "description": "描述",
+            "address": "城市",
+            "latitude": "經度",
+            "longitude": "緯度",
+            "phone": "電話",
+            "url": "網址",
+            "rating": "評分",
         }
