6afa6b62741a2f2438074baf371be7204b316c50
FangFangss <cjiyvonne@gmail.com>
2024-05-20 13:11:45 +0800

    Rename the parameter name of index.js

--- src/scripts/schedules/index.js
+++ src/scripts/schedules/index.js
@@ -1,26 +1,25 @@
 // 頁籤顯示與隱藏功能
-function openTab(tabName, btnId) {
-    let i, tabContent;
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
+function showTab(selectedDate, selectedButtonId) {
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    //隱藏所有日期頁籤內容
+    for (let i = 0; i < tabContents.length; i++) {
+      tabContents[i].style.display = "none";
     }
     // 顯示該日期頁籤的景點
-    const scheduleTab = document.getElementById("schedule" + tabName);
-    if (scheduleTab) {
-        scheduleTab.style.display = "block";
+    const dateTabContent = document.querySelector("#schedule" + selectedDate);
+    if (dateTabContent) {
+      dateTabContent.style.display = "block";
     }
-
     // 移除所有頁籤的顏色
-    const buttons = document.querySelectorAll('.tab');
+    const buttons = document.querySelectorAll(".tab");
     buttons.forEach(button => {
-      button.classList.remove('active-tab');
+      button.classList.remove("active-tab");
     });
     
     // 添加被選中頁籤的顏色
-    const btn = document.querySelector('#' + btnId);
+    const btn = document.querySelector("#" + selectedButtonId);
     btn.classList.add("active-tab");
   }
 
-
-export default openTab
+export default showTab
