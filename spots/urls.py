1333b688e2c777edd5bab3eced3ba8eabaf3ef91
末次 <syunjulee@gmail.com>
2024-05-20 12:01:26 +0800

    Issue #40 frontEnd: heart&fetch, backEnd: add MemberSpot

--- spots/urls.py
+++ spots/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ShowView, IndexView, CreateView
+from .views import ShowView, IndexView, CreateView, toggle_favorite
 from . import views
 
 app_name = "spots"
@@ -9,4 +9,5 @@ urlpatterns = [
     path("new", CreateView.as_view(), name="new"),
     path("<int:pk>/show", ShowView.as_view(), name="show"),
     path("<int:pk>/show/add", views.add, name="add"),
+    path("<int:pk>/toggle_favorite", toggle_favorite, name="toggle_favorite"),
 ]
