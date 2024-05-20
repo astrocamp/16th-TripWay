395d5907108941ac6ba81d1246003efc639cea99
末次 <syunjulee@gmail.com>
2024-05-20 16:16:14 +0800

    Issue #40 fixed: urls&views

--- spots/urls.py
+++ spots/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path, include
-from .views import ShowView, IndexView, CreateView, toggle_favorite
-from . import views
+from django.urls import path
+from .views import ShowView, IndexView, CreateView, toggle_favorite, add
 
 app_name = "spots"
 
@@ -8,6 +7,6 @@ urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("new", CreateView.as_view(), name="new"),
     path("<int:pk>/show", ShowView.as_view(), name="show"),
-    path("<int:pk>/show/add", views.add, name="add"),
+    path("<int:pk>/show/add", add, name="add"),
     path("<int:pk>/toggle_favorite", toggle_favorite, name="toggle_favorite"),
 ]
