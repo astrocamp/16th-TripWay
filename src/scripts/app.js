04d7f837e5e376355ead992782cbb67bcefea7e9
末次 <syunjulee@gmail.com>
2024-05-20 18:33:55 +0800

    Issue #40 fixed: use if in favorite&delete map app

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,14 @@
 import Toast from "./message.js"
 import openTab from "./schedules/index.js"
-import endTimeInputValidation from "./schedules/update.js"
-import "./map.js"
+import "./schedules/update.js"
 import Alpine from "alpinejs"
-import handleDateChange from "./trips/new.js"
+import "./trips/new.js"
 import toggleFavorite from "./favorite.js"
 
 window.Toast = Toast
 window.openTab = openTab
-window.initMap = initMap
 window.Alpine = Alpine
 
 Alpine.start()
-window.endTimeInputValidation = endTimeInputValidation
-window.handleDateChange = handleDateChange
+
 window.toggleFavorite = toggleFavorite
