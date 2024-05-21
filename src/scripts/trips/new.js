6b19482640abe9ec3b19efdfd518b5060130b32a
FangFangss <cjiyvonne@gmail.com>
2024-05-21 09:56:41 +0800

    Added if statement in new.js

--- src/scripts/trips/new.js
+++ src/scripts/trips/new.js
@@ -1,10 +1,13 @@
-function handleDateChange() {
-    // 當出發日期發生變化時觸發
-    document.getElementById("start_date").addEventListener("change", function() {
-        // 獲取出發日期的值
-        let startDateValue = this.value;
-        // 更新結束日期的最小值
-        document.getElementById("end_date").min = startDateValue;
-    });
+const handleDateChange = document.querySelector("#start_date")
+
+if (handleDateChange) {
+    (function () {
+        handleDateChange.addEventListener("change", function() {
+            // 獲取出發日期的值
+            let startDateValue = this.value;
+            // 更新結束日期的最小值
+            document.querySelector("#end_date").min = startDateValue;
+        });
+    })()
 }
-export default handleDateChange; 
+
