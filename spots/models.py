f4d0c96db0c2cae24ce4aa16b6fbc920132e92f2
chenfarng <chenfarng@gmail.com>
2024-05-21 15:37:42 +0800

    Spot name: unique, seed script

--- spots/models.py
+++ spots/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Spot(models.Model):
-    name = models.CharField(max_length=1000)
+    name = models.CharField(max_length=1000, unique=True)
     address = models.CharField(max_length=255, null=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
