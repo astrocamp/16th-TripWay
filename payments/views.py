c31b9a34a1de6e9b8c838e6491784fec2a0b9d7d
Josh Lin <mingyi1223@gmail.com>
2024-05-21 22:38:33 +0800

    Issue#47 update payments views.py

--- payments/views.py
+++ payments/views.py
@@ -1,28 +1,9 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-from django.utils import timezone
-from django.conf import settings
-import binascii
-import hashlib
-from urllib.parse import urlencode
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
 from .service import PaymentService
 
 # Create your views here.
-
-
-MerchantID = settings.ENCRYPTION_KEY["MERCHANT_ID"]
-HASHKEY = settings.ENCRYPTION_KEY["HASH_KEY"]
-HASHIV = settings.ENCRYPTION_KEY["HASH_IV"]
-Version = settings.ENCRYPTION_KEY["VERSION"]
-ReturnUrl = settings.ENCRYPTION_KEY["RETURN_URL"]
-NotifyUrl = settings.ENCRYPTION_KEY["NOTIFY_URL"]
-PayGateWay = settings.ENCRYPTION_KEY["PAY_GATEWAY"]
-RespondType = settings.ENCRYPTION_KEY["RESPOND_TYPE"]
-
 
 def upgrade(request):
     return render(request, "payments/upgrade.html")
