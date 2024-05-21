be8bd1724ca0cdc088cd3463940e880577fcd01b
FangFangss <cjiyvonne@gmail.com>
2024-05-21 10:05:51 +0800

    Remove unneeded code in app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,23 +1,13 @@
 import Toast from "./message.js"
 import showTab from "./schedules/index.js"
-import endTimeInputValidation from "./schedules/update.js"
-import "./map.js"
+import "./schedules/update.js"
+// import "./map.js"
 import Alpine from "alpinejs"
-import handleDateChange from "./trips/new.js"
+import "./trips/new.js"
 
 window.Toast = Toast
 window.showTab = showTab
-window.initMap = initMap
+// window.initMap = initMap
 window.Alpine = Alpine
 
 Alpine.start()
-window.endTimeInputValidation = endTimeInputValidation
-window.handleDateChange = handleDateChange
-
-document.addEventListener("DOMContentLoaded", function() {
-    handleDateChange();
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    endTimeInputValidation();
-});
