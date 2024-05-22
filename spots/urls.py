0ff3b0532ebde2402e5efea5fb83ea26422b99f0
Yuting Lin <ylin8266@gmail.com>
2024-05-22 20:06:17 +0800

    issue #3 modified urls.py import

--- spots/urls.py
+++ spots/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("new", CreateView.as_view(), name="new"),
     path("<int:pk>/show", ShowView.as_view(), name="show"),
     path("<int:pk>/show/add", add, name="add"),
-    path("search/", views.search, name="search"),
-    path("save_spot/", views.save_spot, name="save_spot"),
-    path("delete/<str:spot_id>/", views.delete, name="delete"),
+    path("search/", search, name="search"),
+    path("save_spot/", save_spot, name="save_spot"),
+    path("delete/<str:spot_id>/", delete, name="delete"),
     path("<int:pk>/toggle_favorite", toggle_favorite, name="toggle_favorite"),
 ]
