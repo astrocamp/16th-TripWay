04cdd939295fc2d1e39edbe2448431ccef8b5575
FangFangss <cjiyvonne@gmail.com>
2024-05-22 13:10:30 +0800

    Remove the new path of urls.py

--- schedules/urls.py
+++ schedules/urls.py
@@ -6,7 +6,6 @@ app_name = "schedules"
 urlpatterns = [
     path("", views.index, name="index"),
     path("add_day", views.add_day, name="add_day"),
-    path("new", views.new, name="new"),
     path("add", views.create, name="add"),
     path("<id>/update", views.update, name="update"),
     path("<id>/delete", views.delete, name="delete"),
