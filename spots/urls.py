164de2d15a1d39f683a05b0ffef775ac876fb51b
Josh Lin <mingyi1223@gmail.com>
2024-05-23 14:56:24 +0800

    Issue#65 update setting & spots func

--- spots/urls.py
+++ spots/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ShowView, IndexView, CreateView, toggle_favorite, add
+from .views import ShowView, IndexView, CreateView, toggle_favorite, add_schedule
 
 app_name = "spots"
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("new", CreateView.as_view(), name="new"),
     path("<int:pk>/show", ShowView.as_view(), name="show"),
-    path("<int:pk>/show/add", add, name="add"),
+    path("<int:pk>/show/add", add_schedule, name="add_schedule"),
     path("<int:pk>/toggle_favorite", toggle_favorite, name="toggle_favorite"),
 ]
