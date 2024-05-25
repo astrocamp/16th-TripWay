be3f106acaeaaac741a58525e2a9a20f3f512b05
FangFangss <cjiyvonne@gmail.com>
2024-05-25 12:17:18 +0800

    Add login_required in schedules/views.py

--- schedules/views.py
+++ schedules/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from trips.models import Trip, TripMember
 from .models import Schedule
 from datetime import timedelta
@@ -10,11 +11,11 @@ from operator import attrgetter
 from members.models import Member
 from spots.models import Spot
 
+@login_required
 def index(request, id):
     trip = get_object_or_404(Trip, pk=id)
-    schedules = Schedule.objects.filter(trip=trip.id, deleted_at=None).order_by(
-        "date", "start_time"
-    )
+    schedules = Schedule.objects.filter(trip=trip.id, deleted_at=None).order_by("date", "start_time")
+
     grouped_schedules = {}
     # 根據行程的日期屬性將行程分組並提取日期
     for date, group in groupby(schedules, key=attrgetter("date")):
@@ -24,12 +25,19 @@ def index(request, id):
     member_ids = TripMember.objects.filter(trip_id=id).order_by("id").values_list("member_id", flat=True)
     members = Member.objects.filter(id__in=member_ids)
     trip_member = get_object_or_404(TripMember, trip=trip, member=request.user)
+    
     return render(
-        request, "schedules/index.html", {"schedule_dates": grouped_schedules, "date_range": date_range, "trip": trip, "members": members, "trip_member": trip_member}
+        request, "schedules/index.html", {
+            "schedule_dates": grouped_schedules, 
+            "date_range": date_range, 
+            "trip": trip, "members": members, 
+            "trip_member": trip_member
+        }
     )
 
 
 @require_POST
+@login_required
 def add_day(request, id):
     trip = get_object_or_404(Trip, pk=id)
     trip.end_date = trip.end_date + timedelta(days=1)
@@ -38,6 +46,7 @@ def add_day(request, id):
 
 
 @require_POST
+@login_required
 def create(request):
     trip_id = request.POST.get("trip_id")
     trip = get_object_or_404(Trip, id=trip_id)
@@ -58,6 +67,7 @@ def create(request):
     return redirect(f"/trips/{trip.id}/schedules/")
 
 
+@login_required
 def show(request, id):
     schedules = get_object_or_404(Schedule, pk=id)
     trip = schedules.trip
@@ -77,10 +87,14 @@ def show(request, id):
             return redirect("schedules:update", id=id) 
     else:
         trip_member = get_object_or_404(TripMember, trip=trip, member=request.user)
-        return render(request, "schedules/show.html", {"schedules": schedules, "trip": trip, "trip_member": trip_member}
-        )
+        return render(request, "schedules/show.html", {
+            "schedules": schedules, 
+            "trip": trip, 
+            "trip_member": trip_member
+        })
 
 
+@login_required
 def update(request, id):
     schedule = get_object_or_404(Schedule, pk=id)
     date_range = schedule.trip.get_date_range()
@@ -90,6 +104,7 @@ def update(request, id):
     )
 
 
+@login_required
 @require_POST
 def delete(request, id):
     schedule = get_object_or_404(Schedule, pk=id)
