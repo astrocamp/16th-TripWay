e7512bca141a3632fc5573009ac9e452a13e3c0c
FangFangss <cjiyvonne@gmail.com>
2024-05-25 12:16:28 +0800

    Add login_required in payments/views.py

--- payments/views.py
+++ payments/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from .service import PaymentService
 
 # Create your views here.
 
+@login_required
 def upgrade(request):
     return render(request, "payments/upgrade.html")
 
 
 @require_POST
+@login_required
 def create_order(request):
     member = request.user
     price = request.POST["price"]
@@ -17,6 +20,7 @@ def create_order(request):
 
 
 @csrf_exempt
+@login_required
 def newpay_return(request):
     print(request)
     return render(request, "payments/success.html")
