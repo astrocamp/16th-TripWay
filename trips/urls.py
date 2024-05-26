d81d4c2b2c9540dd6db756d5387e1ac7162be04e
Josh Lin <mingyi1223@gmail.com>
2024-05-27 00:48:10 +0800

    Issue#70 add trips/update.html & modify views

--- trips/urls.py
+++ trips/urls.py
@@ -4,16 +4,15 @@ from . import views
 app_name = "trips"
 
 urlpatterns = [
-    path("", views.home, name="index"),  
-    path("new", views.new, name="new"),  
-    path("map", views.map, name="map"),  
-    path("add", views.create, name="add"),  
-    path("delete/<id>/", views.delete, name="delete"),  
-    path("<id>/schedules/", include("schedules.urls")),  
+    path("", views.home, name="index"),
+    path("new", views.new, name="new"),
+    path("map", views.map, name="map"),
+    path("add", views.create, name="add"),
+    path("<id>/update/", views.update, name="update"),
+    path("<id>/delete/", views.delete, name="delete"),
+    path("<id>/schedules/", include("schedules.urls")),
     path("<id>/new-member", views.new_member, name="new-member"),
     path("<id>/add-member", views.create_member, name="add-member"),
     path("<trip_id>/<member_id>/delete-member", views.delete_member, name="delete-member"),
     path("<trip_id>/<member_id>/delete-self", views.delete_self, name="delete-self"),
-    # path("upload_photo", views.upload_photo, name="upload_photo"),
-    # path("delete_photo", views.delete_photo, name="delete_photo"), 
 ]
