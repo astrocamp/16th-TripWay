b5ce8220f832da24033797435406223edbd478dd
Josh Lin <mingyi1223@gmail.com>
2024-05-27 14:16:44 +0800

    Issue#70 modify delete-photo js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,20 +12,3 @@ window.showTab = showTab
 window.toggleFavorite = toggleFavorite
 window.Swal = Swal;
 
-document
-  .getElementById("delete-photo-btn")
-  .addEventListener("click", function () {
-    document.getElementById("delete-photo-modal").style.display = "block";
-  });
-
-document
-  .getElementById("confirm-delete-btn")
-  .addEventListener("click", function () {
-    document.getElementById("delete-form").submit();
-  });
-
-document
-  .getElementById("cancel-delete-btn")
-  .addEventListener("click", function () {
-    document.getElementById("delete-photo-modal").style.display = "none";
-  });
