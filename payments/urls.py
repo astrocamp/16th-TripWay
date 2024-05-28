17c7f52fdd68055870d9c910c51f7552d695ffb5
Josh Lin <mingyi1223@gmail.com>
2024-05-29 01:26:03 +0800

    fix trips.views & upgrade.html % payments return login

--- payments/urls.py
+++ payments/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.upgrade, name="upgrade"),
     path("create", views.create_order, name="create"),
     path("return", views.newpay_return, name="return"),
+    path("transition", views.transition, name="transition"),
 ]
--- trips/views.py
+++ trips/views.py
@@ -4,9 +4,9 @@ from django.urls import reverse
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Trip, TripMember
 from django.conf import settings
-from django.contrib import messages
+from members.models import Member
+from .models import Trip, TripMember
 
 
 @login_required
