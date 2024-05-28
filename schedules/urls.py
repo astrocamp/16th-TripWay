a5ac4ce41c0f9b24b4c20b86ec6f8bf82ecc1ebe
Yuting Lin <ylin8266@gmail.com>
2024-05-28 11:30:01 +0800

    issue #3 update route map

--- schedules/urls.py
+++ schedules/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.create, name="add"),
     path("add_day", views.add_day, name="add_day"),
+    path("get_schedule/", views.get_schedule, name="get_schedule"),
     path("<id>/update", views.update, name="update"),
     path("<id>/delete", views.delete, name="delete"),
     path("<id>/show", views.show, name="show"),
