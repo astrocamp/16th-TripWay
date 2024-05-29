4702a75e5b008b4d57d230939951f6adef5251bb
chenfarng <chenfarng@gmail.com>
2024-05-29 16:51:50 +0800

    install alpinejs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,10 +4,12 @@ import "./schedules/update.js"
 import "./trips/new.js"
 import toggleFavorite from "./favorite.js"
 import Swal from "sweetalert2";
-
+import Alpine from "alpinejs";
 
 window.Toast = Toast
+window.Alpine = Alpine
 window.showTab = showTab
 window.toggleFavorite = toggleFavorite
 window.Swal = Swal;
 
+Alpine.start();
