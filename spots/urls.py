a3af571ee70fda37c47af5407441f48920b937aa
Yuting Lin <ylin8266@gmail.com>
2024-05-31 02:37:40 +0800

    issue #73 add url for search & modified map

--- spots/urls.py
+++ spots/urls.py
@@ -13,6 +13,7 @@ app_name = "spots"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("new", CreateView.as_view(), name="new"),
+    path("search/", SearchView.as_view(), name="search"),
     path("<int:pk>/show", ShowView.as_view(), name="show"),
     path("<int:pk>/show/add", add_schedule, name="add_schedule"),
     path("<int:pk>/favorite", toggle_favorite, name="favorite"),
