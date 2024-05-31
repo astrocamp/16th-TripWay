96f132adcd2e2e0a6bfd2ed01ae134a301383d6c
FangFangss <cjiyvonne@gmail.com>
2024-05-31 14:05:43 +0800

    Issue #96 added button color in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "light-blue": "#85B6FF",
-        blue: "#EBF0F6",
-        "btn-blue": "#649DFF",
-      },
+        'light-blue': '#85B6FF',
+        'blue': '#EBF0F6',
+        'blue-btn': '#00A0B0',
+        'blue-dc': '#005a64',
+        'btn-blue': '#649DFF',
+      }
     },
   },
   plugins: [require("daisyui")],
