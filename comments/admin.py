84b45307f228be40fa156f30f41e5135d36294a3
Sean Chen <seanchen1073@gmail.com>
2024-05-31 16:56:34 +0800

    Issue #94 modified admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-<<<<<<< HEAD
 from .models import Comment
 
 admin.site.register(Comment)
-=======
 
-# Register your models here.
->>>>>>> d89066a (create comments and rating stystem with CRUD)
-=======
-from .models import Comment
 
-admin.site.register(Comment)
->>>>>>> 54e9969 (modified comments)
