5b76c5759a281d77686fdd6c867e20a72851dc4b
Josh Lin <mingyi1223@gmail.com>
2024-06-01 11:01:15 +0800

    modify footer CDN

--- src/scripts/trips/uploadPic.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("delete-photo-btn").addEventListener("click", function () {
-  document.getElementById("delete-photo-modal").style.display = "block";
-});
-
-document.getElementById("confirm-delete-btn").addEventListener("click", function () {
-  document.getElementById("delete-form").submit();
-});
