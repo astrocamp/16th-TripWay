07beb908452fe86ea6da593218537d10380bba92
Yuting Lin <ylin8266@gmail.com>
2024-06-02 17:30:51 +0800

    issue #105 alter nav/base name

--- comments/migrations/0001_initial.py
+++ comments/migrations/0001_initial.py
@@ -10,19 +10,42 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('spots', '0001_initial'),
+        ("spots", "0001_initial"),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Comment',
+            name="Comment",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('content', models.TextField()),
-                ('value', models.IntegerField(default=None)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('spot', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='spots.spot')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("content", models.TextField()),
+                ("value", models.IntegerField(default=None)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "spot",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="spots.spot",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
