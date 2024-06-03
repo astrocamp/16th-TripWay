cfc73223961d0c650f0e7e1c5e901823cb4ec55e
FangFangss <cjiyvonne@gmail.com>
2024-06-04 06:12:45 +0800

    Issue #103 add app_name in notifies/urls.py

--- notifies/urls.py
+++ notifies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "notifies"
+
 urlpatterns = [
     path("", views.notification_list, name='notification_list'),
     path("delete/<int:notification_id>/", views.delete_notification, name="delete_notification"),
