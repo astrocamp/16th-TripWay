6cf0b60d2c6884d8cd25bf8d5723a06a4aa0aa87
FangFangss <cjiyvonne@gmail.com>
2024-06-03 21:55:01 +0800

    Issue #103 update notifies/models.py

--- notifies/models.py
+++ notifies/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from django.conf import settings
+from members.models import Member
+from trips.models import Trip
 
 class Notification(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, null=True, blank=True)
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
