64996d3fb0b3e02a366575f9c3cdb81508cd74df
FangFangss <cjiyvonne@gmail.com>
2024-06-03 21:53:54 +0800

    Issue #103 update notifies/urls.py

--- notifies/urls.py
+++ notifies/urls.py
@@ -1,8 +1,8 @@
-# notifies/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.notify_list, name='notify_list'),
+    path("", views.notification_list, name='notification_list'),
+    path("delete/<int:notification_id>/", views.delete_notification, name="delete_notification"),
     path("mark_as_read/<int:notification_id>/", views.mark_as_read, name="mark_as_read"),
 ]
