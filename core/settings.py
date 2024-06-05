2e0111a95a281a89d6482fc55c579afae408ea02
chenfarng <chenfarng@gmail.com>
2024-06-05 12:11:46 +0800

    setting DATABASES

--- core/settings.py
+++ core/settings.py
@@ -103,11 +103,11 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "TripWay",
-        "USER": os.getenv("YOUR_USER_NAME"),
-        "PASSWORD": os.getenv("YOUR_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
