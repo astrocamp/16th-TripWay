2ee842f1d0cbdadbe4216d2d1239225142bf0dad
chenfarng <chenfarng@gmail.com>
2024-06-05 18:21:05 +0800

    MIDDLEWARE

--- core/settings.py
+++ core/settings.py
@@ -90,6 +90,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
