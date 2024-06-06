4dc707a4e8c1e413d5b7a6cb9b7847c3b4f010fe
chenfarng <chenfarng@gmail.com>
2024-06-06 17:47:18 +0800

    temp set debug

--- core/settings.py
+++ core/settings.py
@@ -18,10 +18,7 @@ SECRET_KEY = "django-insecure-&u@2d$fi47oaawy#os8ak*nn7zx_g7e$!ci*w=&i(8#j^xs@%^
 IS_HEROKU_APP = os.getenv("HEROKU_APP")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if IS_HEROKU_APP:
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
 HOST_NAME = os.getenv("HOST_NAME")
 CSRF_TRUSTED_ORIGINS = [f"https://{HOST_NAME}"]
