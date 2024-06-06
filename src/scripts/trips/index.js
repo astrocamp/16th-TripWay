6064e687015645fe1304dd48b8030c8e6510eed7
Josh Lin <mingyi1223@gmail.com>
2024-06-06 14:45:50 +0800

    Issue#121 move js to index.js

--- src/scripts/trips/index.js
+++ src/scripts/trips/index.js
@@ -31,3 +31,51 @@ function toggleMenuVisibility() {
 }
 
 document.addEventListener("DOMContentLoaded", toggleMenuVisibility);
+
+const copyEditButton = document.querySelector("#copyEditButton")
+const copyWatchButton = document.querySelector("#copyWatchButton")
+
+function copyEdit(){
+    document.getElementById('copyEditButton').addEventListener('click', function(e) {
+        const copyURL = e.target.querySelector("span").textContent;
+
+        // 使用 Clipboard API 來複製
+        navigator.clipboard.writeText(copyURL).then(() => {
+
+            var successMessage = document.getElementById('successMessage2');
+            successMessage.style.display = 'block';
+
+            setTimeout(() => {
+                successMessage.style.display = 'none';
+            }, 2000);
+        }).catch(err => {
+            console.error('複製失敗: ', err);
+        })
+    })
+}
+ 
+function copyWatch(){
+    document.getElementById('copyWatchButton').addEventListener('click', function(e) {
+        const copyURL = e.target.querySelector("span").textContent;
+
+        navigator.clipboard.writeText(copyURL).then(() => {
+
+            var successMessage = document.getElementById('successMessage');
+            successMessage.style.display = 'block';
+
+            setTimeout(() => {
+                successMessage.style.display = 'none';
+            }, 2000);
+        }).catch(err => {
+            console.error('複製失敗: ', err);
+        });
+    });
+}
+
+if (copyEditButton){
+    copyEdit();
+}
+
+if (copyWatchButton){
+    copyWatch();
+}
