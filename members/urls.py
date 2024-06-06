5ab2961e5c2b34bcbfe6dc6f9f662787fdacc166
末次 <syunjulee@gmail.com>
2024-06-06 16:55:14 +0800

    Issue #80 update: favorite icon can show profile page and work

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from spots.views import toggle_favorite
 
 urlpatterns = [
     path("login", views.login_user, name="login"),
     path("logout", views.logout_user, name="logout"),
     path("register", views.register_user, name="register"),
+    path("spots/<spot_id>/favorite/", toggle_favorite, name="favorite"),
     path("profile", views.profile, name="profile"),
 ]
