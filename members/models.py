89f719e9b023a3a6f0c4e138b4fbdde55a535554
Yuting Lin <ylin8266@gmail.com>
2024-06-06 18:17:41 +0800

    issue 105 add function for profile uploading to models/urls/views

--- members/models.py
+++ members/models.py
@@ -6,13 +6,14 @@ from spots.models import Spot
 class Member(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     level = models.CharField(max_length=10, default="基本會員")
+    image = models.ImageField(null=True, default=None, upload_to="trips_coverPhoto/")
 
 
 class MemberSpot(models.Model):
-    member = models.ForeignKey(
-        Member, on_delete=models.CASCADE, related_name="members"
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="members")
+    spot = models.ForeignKey(
+        Spot, on_delete=models.CASCADE, related_name="favorite_spots"
     )
-    spot = models.ForeignKey(Spot, on_delete=models.CASCADE, related_name="favorite_spots")
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
