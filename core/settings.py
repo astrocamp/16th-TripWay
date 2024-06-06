1497d8ec8205de948fe6039ef8503c804de5c0d1
chenfarng <chenfarng@gmail.com>
2024-06-07 06:53:15 +0800

    set static

--- core/settings.py
+++ core/settings.py
@@ -178,8 +178,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+if IS_HEROKU_APP:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
