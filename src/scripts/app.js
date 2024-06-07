8e80e134f1371479e93c87a02a823a7deac49405
末次 <syunjulee@gmail.com>
2024-06-07 13:32:09 +0800

    Issue #125 update: profile tab

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,7 @@
 import Toast from "./message.js"
 import showTab from "./schedules/index.js"
+import "./notice.js"
+import "./profile/tab.js"
 import "./profile/upload.js"
 import "./notice.js"
 import "./navbar.js"
@@ -8,13 +10,13 @@ import "./spots/comments.js"
 import "./trips/index.js"
 import "./trips/new.js"
 import toggleFavorite from "./favorite.js"
-import Swal from "sweetalert2";
-import Alpine from "alpinejs";
+import Swal from "sweetalert2"
+import Alpine from "alpinejs"
 
 window.Toast = Toast
 window.Alpine = Alpine
 window.showTab = showTab
 window.toggleFavorite = toggleFavorite
-window.Swal = Swal;
+window.Swal = Swal
 
-Alpine.start();
+Alpine.start()
