39ffd4d3f2fb3253c67cccebe0e724366c323884
FangFangss <cjiyvonne@gmail.com>
2024-06-08 18:54:12 +0800

    Fix bug in function of compressing images

--- trips/models.py
+++ trips/models.py
@@ -4,7 +4,6 @@ from members.models import Member
 from datetime import timedelta
 from PIL import Image
 from io import BytesIO
-from django.core.files.base import ContentFile
 
 
 class Trip(models.Model):
@@ -19,24 +18,17 @@ class Trip(models.Model):
 
     def get_date_range(self):
         return [
-            self.start_date + timedelta(days=x)
+            self.start_date + timedelta(days=x) 
             for x in range((self.end_date - self.start_date).days + 1)
         ]
-
-    def save(self, *args, **kwargs):
-        if self.image:
-            img = Image.open(self.image)
-            max_size = (600, 600)
-            img.thumbnail(max_size, Image.LANCZOS)
-            thumb_io = BytesIO()
-            img_format = "PNG" if img.mode == "RGBA" else "JPEG"
-            img.save(thumb_io, format=img_format)
-            thumb_io.seek(0)
-            file_extension = "png" if img.mode == "RGBA" else "jpg"
-            new_file_name = f"{self.image.name.split('.')[0]}_thumb.{file_extension}"
-            self.image.save(new_file_name, ContentFile(thumb_io.read()), save=False)
-        super().save(*args, **kwargs)
-
+    
+    def compress_image(image):
+        img = Image.open(image)
+        img.thumbnail((200, 200))
+        output = BytesIO()
+        img.save(output, format="PNG", quality=70)
+        output.seek(0)
+        return output
 
 class TripMember(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
