cf6e2cc9da6b736c9de1254ce59906b42d0aae89
Sean Chen <seanchen1073@gmail.com>
2024-06-10 04:14:28 +0800

    Issue #149 added edit article feature

--- blogs/views.py
+++ blogs/views.py
@@ -7,7 +7,7 @@ from .models import Blog, BlogComment
 @login_required
 def index(request):
     blogs = Blog.objects.all()
-    return render(request, 'blogs/index.html', {'blogs': blogs})
+    return render(request, 'blogs/index.html', {'blogs': blogs, 'user': request.user})
 
 @login_required
 def article(request, blog_id):
@@ -31,7 +31,9 @@ def new(request):
     if request.method == 'POST':
         form = BlogForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            blog = form.save(commit=False)
+            blog.user = request.user
+            blog.save()
             messages.success(request, "文章已成功建立！")
             return redirect('blogs:index')
         else:
@@ -39,3 +41,32 @@ def new(request):
     else:
         form = BlogForm()
     return render(request, 'blogs/new.html', {'form': form})
+
+@login_required
+def edit(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if blog.user != request.user:
+        messages.error(request, "您沒有權限編輯此文章。")
+        return redirect('blogs:index')
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "文章已成功更新！")
+            return redirect('blogs:article', blog_id=blog_id)
+        else:
+            messages.error(request, "表單有誤，請檢查輸入內容。")
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'blogs/edit.html', {'form': form, 'blog': blog})
+
+@login_required
+def delete_blog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if blog.user != request.user:
+        messages.error(request, "您沒有權限刪除此文章。")
+    else:
+        blog.delete()
+        messages.success(request, "文章已刪除！")
+    return redirect('blogs:index')
