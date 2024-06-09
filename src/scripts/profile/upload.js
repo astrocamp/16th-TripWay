683fae9c1a60553c2787d01bb2664234c7c740a3
Josh Lin <mingyi1223@gmail.com>
2024-06-10 03:19:03 +0800

    Issue#153 update profile image

--- src/scripts/profile/upload.js
+++ src/scripts/profile/upload.js
@@ -1,48 +1,21 @@
-function imageUploader() {
-    const imageInput = document.getElementById("image-profile");
+const imageInput = document.getElementById("image-profile")
+
+
+if (imageInput){
     const imageBtn = document.getElementById("image-btn");
     const submitBtn = document.getElementById("submit-btn");
-    const navAvatar = document.querySelector('.dropdown button');
 
     imageInput.addEventListener("change", function (event) {
         const selectedFile = event.target.files[0];
         if (selectedFile) {
             const reader = new FileReader();
             reader.onload = function (e) {
-
                 imageBtn.classList.add('hidden');
                 submitBtn.classList.remove('hidden');
+                document.querySelector("#image").src = e.target.result
             };
             reader.readAsDataURL(selectedFile);
         }
-    });
-
-    submitBtn.addEventListener("click", function (event) {
-        event.preventDefault();
-
-        const form = document.getElementById("upload-form");
-        const formData = new FormData(form);
-
-        fetch(form.action, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'X-CSRFToken': form.querySelector('[name=csrfmiddlewaretoken]').value
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                navAvatar.style.backgroundImage = `url(${data.imageUrl})`;
-
-                imageInput.value = '';
-                submitBtn.classList.add('hidden');
-                imageBtn.classList.remove('hidden');
-            }
-        })
-        .catch(error => {
-            console.error('Error uploading image:', error);
-        });
     });
 }
 
