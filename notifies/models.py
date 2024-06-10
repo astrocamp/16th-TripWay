a8871d5b40d794daf1094029c0d47fef50992b5f
FangFangss <cjiyvonne@gmail.com>
2024-06-10 17:38:50 +0800

    Issue #156 added type field in notifies/models.py

--- notifies/models.py
+++ notifies/models.py
@@ -6,6 +6,7 @@ class Notification(models.Model):
     user = models.ForeignKey(Member, on_delete=models.CASCADE)
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, null=True, blank=True)
     message = models.CharField(max_length=255)
+    type = models.CharField(max_length=20)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
