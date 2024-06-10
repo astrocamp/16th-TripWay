6b8802a11033d087ec6f8181486b55fc34d15638
FangFangss <cjiyvonne@gmail.com>
2024-06-10 17:41:05 +0800

    Issue #156 add notification link

--- static/scripts/notifications.js
+++ static/scripts/notifications.js
@@ -7,7 +7,7 @@ function markNotificationsAsRead() {
     })
     .then(response => {
         if (response.ok) {
-            document.querySelectorAll(".notification-dot").forEach(dot => dot.style.display = "none");
+            window.location.href = link.getAttribute("href");
         }
     })
     .catch(error => console.error("Error updating notification status:", error));
