5ed463ef051b65771aca6f62f9d4b14c532fe698
末次 <syunjulee@gmail.com>
2024-06-10 12:10:32 +0800

    Issue #125 update: profile tabs and cards

--- members/views.py
+++ members/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import SignUp
-from .models import MemberSpot, Member
-from PIL import Image
-from io import BytesIO
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+
+from PIL import Image
+from io import BytesIO
+
+from .forms import SignUp
+from .models import MemberSpot, Member
+from .models import MemberSpot
+from trips.models import TripMember
 
 
 def login_user(request):
@@ -48,12 +52,36 @@ def register_user(request):
 
     return render(request, "registration/register.html", {"form": form})
 
+
+def get_trip_data(member):
+    trip_members = TripMember.objects.filter(member=member).select_related("trip")
+
+    sort_option = 'created_desc'
+    if sort_option == 'date_asc':
+        trip_members = trip_members.order_by('trip__start_date')
+    elif sort_option == 'date_desc':
+        trip_members = trip_members.order_by('-trip__start_date')
+    else:
+        trip_members = trip_members.order_by('-trip__id')
+
+    trips = [{"t": trip_member.trip, "tm": trip_member } for trip_member in trip_members]
+
+    return trips
+
+
 @login_required
 def profile(request):
     member = request.user
-
     spots = MemberSpot.objects.filter(member=member).select_related("spot")
-    return render(request, "profile/index.html", {"spots": spots, "member": member})
+    trips = get_trip_data(member)
+
+    context = {
+        "spots": spots,
+        "member": member,
+        "trips": trips,
+    }
+
+    return render(request, "profile/index.html", context)
 
 
 def create(request):
--- src/scripts/favorite.js
+++ src/scripts/favorite.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.addEventListener("click", (event) => {
         const favoriteIcon = event.target
         if (favoriteIcon && favoriteIcon.matches(".fa-heart")) {
+            event.stopPropagation()
+            event.preventDefault()
             toggleFavorite(favoriteIcon)
         }
     })
 })
-
-let isFavorite = false
 
 function toggleFavorite(element) {
     const toggleFavoriteUrl = element.dataset.toggleFavoriteUrl
@@ -22,11 +22,9 @@ function toggleFavorite(element) {
     .then(response => response.json())
     .then(data => {
         if (data.status === "added") {
-            isFavorite = true
             favoriteIcon.classList.remove("fa-regular")
             favoriteIcon.classList.add("fa-solid", "text-btn-red")
         } else if (data.status === "removed") {
-            isFavorite = false
             favoriteIcon.classList.remove("fa-solid", "text-btn-red")
             favoriteIcon.classList.add("fa-regular")
         }
