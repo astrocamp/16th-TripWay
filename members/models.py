42b88f03b93c065c2cebd45fcf92e4367bb351b7
Sean Chen <seanchen1073@gmail.com>
2024-06-12 15:43:14 +0800

    Issue #176 removed upload_to='profile_images/',

--- members/models.py
+++ members/models.py
@@ -6,7 +6,7 @@ from spots.models import Spot
 class Member(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     level = models.CharField(max_length=10, default="基本會員")
-    image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
 
 class MemberSpot(models.Model):
