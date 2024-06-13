9c57a10592ef060c641cb308b3191836be0318d2
FangFangss <cjiyvonne@gmail.com>
2024-06-14 04:07:12 +0800

    Issue #204 added order field in schedules/models.py

--- schedules/models.py
+++ schedules/models.py
@@ -25,6 +25,16 @@ class Schedule(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True)
+    order = models.PositiveIntegerField(default=0) 
+
+    def save(self, *args, **kwargs):
+        if self.order == 0:  # 如果 order 為預設值
+            max_order = Schedule.objects.filter(trip=self.trip).aggregate(models.Max('order'))['order__max']
+            if max_order is None:
+                max_order = 0
+            self.order = max_order + 1
+        super(Schedule, self).save(*args, **kwargs)
+
 
     def soft_delete(self):
         self.deleted_at = timezone.now()
@@ -40,3 +50,7 @@ class Schedule(models.Model):
     @property
     def spot_longitude(self):
         return self.spot.longitude if self.spot else None
+
+
+    class Meta:
+        ordering = ["order", "date", "start_time"]  # 按 order 排序
