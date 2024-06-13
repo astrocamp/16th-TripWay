629651a0bbdafdc5442110930d18a2659db6bc98
chenfarng <chenfarng@gmail.com>
2024-06-13 17:36:03 +0800

    issue #198 blog update,  delete

--- blogs/urls.py
+++ blogs/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("article/<int:blog_id>/", views.article, name="article"),
     path("new/", views.new, name="new"),
     path("edit/<int:blog_id>/", views.edit, name="edit"),
+    path("delete/<int:blog_id>/", views.delete_blog, name="delete"),
 ]
